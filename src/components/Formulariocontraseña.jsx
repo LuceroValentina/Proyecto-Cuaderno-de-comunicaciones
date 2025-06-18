62cf07d3528a2d451b7acc65ec791a8d5cd8bb7f
delff-ssj <jazmincarrasco071206@gmail.com>
2025-06-18 04:57:07 -0300

    subiendo pantalla LOGIN

---
+++ src/components/Formulariocontraseña.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import {useNavigate} from "react-router-dom";
+import "../css/Formulariocontraseña.css";
+
+const Formulariocontraseña = ({ titulo = "Formulario Contraseña" }) => {
+    const [contraseña, setContraseña] = useState('');
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()    
+    };
+
+    return (
+        <div className="form-box">
+            <form onSubmit={handleSubmit} className="glass-form">
+                <h2> Formulario Contraseña </h2>
+
+                <div className="form-group">
+                    <label>Contraseña: </label>
+                    <input
+                        type="password"
+                        value={contraseña}
+                        onChange={(e) => setContraseña(e.target.value)}
+                        placeholder="Ingrese su contraseña"
+                    />
+                </div>
+
+                <button type="submit">Enviar</button>
+            </form>
+        </div>
+    );
+};
+
+export default Formulariocontraseña;
