cb8ab57d901bb0d45157aadfbcde07d109563459
LuceroValentina <lucero.valentina.epet20@gmail.com>
2025-08-19 17:03:12 -0300

    Cambiando botones de PantallaTeoria por menú lateral desplegable

--- src/components/PantallaTeoria.jsx
+++ src/components/PantallaTeoria.jsx
@@ -1,30 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/PantallaTeoria.css";
 import "../css/Elementos.css";
 
 const PantallaTeoria = () => {
-
+    const [activo, setActivo] = useState(false);
     return (
         <div className="container">
-            <div className="titulo">
-            <h1>
-                Teoria
-            </h1>
-            </div>
-            <div className="botones">
-            <button className="botonOscuro">Retiros</button>
-            <button className="botonOscuro">Evaluaciones y TPS</button>
-            <button className="botonOscuro">Comunicación General</button>
-            <button className="botonOscuro">Registros de Firmas</button>
-            <button className="botonOscuro">Horarios de Clases</button>
-            <button className="botonOscuro">Vestimenta</button>
-            </div>
-            <div className="cantenedorbotones">
-                <button className="botonOscuro">Resumen Cuatrimestre</button>
-                <button className="botonOscuro">Calificaciones</button>
-                <button className="botonOscuro">Periodos Exámenes</button>
-                <button className="botonOscuro">Clases de Consultas</button>
-                <button className="botonOscuro">Inasistencias</button>
+            <h1 className="titulo">Teoría</h1>
+            <button className="boton-abrir" onClick={() => setActivo(!activo)}>Abrir</button>
+            <div className={`container-menu ${activo ? "activo" : ""}`}>
+                <div className="container-items">
+                    <h2 className="subtitulo">Menú</h2>
+                    <ul>
+                        <li className="lista"><a href="#" className="item">Retiros</a></li>
+                        <li className="lista"><a href="#" className="item">Evaluaciones y TPS</a></li>
+                        <li className="lista"><a href="#" className="item">Comunicación General</a></li>
+                        <li className="lista"><a href="#" className="item">Registro de firmas</a></li>
+                        <li className="lista"><a href="#" className="item">Horarios de Clase</a></li>
+                        <li className="lista"><a href="#" className="item">Vestimenta</a></li>
+                        <li className="lista"><a href="#" className="item">Inasistencias</a></li>
+                        <li className="container-submenu">Resumen Cuatrimestre
+                            <ul className="submenu">
+                                <li className="lista-submenu"><a href="#" className="item">1er Cuatrimestre</a></li>
+                                <li className="lista-submenu"><a href="#" className="item">2do Cuatrimestre</a></li>
+                            </ul>
+                        </li>
+                        <li className="container-submenu">Calificaciones
+                            <ul className="submenu">
+                                <li><a href="#" className="item">Evaluaciones</a></li>
+                                <li><a href="#" className="item">Láminas</a></li>
+                            </ul>
+                        </li>
+                        <li className="container-submenu">Periodos Exámenes
+                            <ul className="submenu">
+                                <li><a href="#" className="item">POEC</a></li>
+                                <li><a href="#" className="item">Mesas Regulares</a></li>
+                                <li><a href="#" className="item">Mesas Previas</a></li>
+                            </ul>
+                        </li>
+                        <li className="container-submenu">Clases de Consultas
+                            <ul className="submenu">
+                                <li><a href="#" className="item">Horarios</a></li>
+                                <li><a href="#" className="item">Asistencias</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <button className="boton-cerrar" onClick={() => setActivo(!activo)}>
+                    Cerrar</button>
             </div>
             <div className="botonesClaros">
                 <button className="botonClaro">Crear</button>
