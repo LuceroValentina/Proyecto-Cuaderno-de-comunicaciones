10b121f14ab1bc71f3b9ae62c314e6f9c44630d1
LuceroValentina <lucero.valentina.epet20@gmail.com>
2025-08-26 14:30:14 -0300

    Subiendo alineacion con display grid de botones en PantallaSecciones

--- src/components/PantallaSecciones.jsx
+++ src/components/PantallaSecciones.jsx
@@ -8,24 +8,24 @@ const PantallaSecciones = () => {
     return (
         <div className='container-secciones'>
             <h1 className='titulo'>Seleccione la Sección:</h1>
-        <div className='container-botones'>
-            <button className="botonOscuro" onClick={() => navigate("/seccion_teoria")}>
-                Teoría
-            </button>
-            <button className="botonOscuro" onClick={() => navigate("/seccion_taller")}>
-                Taller
-            </button>
-            <button className="botonOscuro" onClick={() => navigate("/seccion_biblioteca")}>
-                Biblioteca
-            </button>
-            <button className="botonOscuro" onClick={() => navigate("/seccion_edfisica")}>
-                Ed.física
-            </button>        
-            
-            </div>   
+            <div className='container-botones'>
+                <button className="botonOscuro" onClick={() => navigate("/seccion_teoria")}>
+                    Teoría
+                </button>
+                <button className="botonOscuro" onClick={() => navigate("/seccion_taller")}>
+                    Taller
+                </button>
+                <button className="botonOscuro" onClick={() => navigate("/seccion_biblioteca")}>
+                    Biblioteca
+                </button>
+                <button className="botonOscuro" onClick={() => navigate("/seccion_edfisica")}>
+                    Ed.física
+                </button>
+
+            </div>
             <button className="contactanos" onClick={() => navigate("/seccion_contactos")}>
-            Contactanos
-            </button>  
+                Contactanos
+            </button>
         </div>
     );
 }
