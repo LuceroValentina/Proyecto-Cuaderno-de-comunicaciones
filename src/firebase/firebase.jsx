76c461bc76cd3ec1964d8348ba6e6db8663e3aaf
Guadalupe-Git <carrasqueraguadalupe@gmail.com>
2025-08-27 20:57:43 -0300

    Arreglado errores de la pantalla de login

--- src/firebase/firebase.jsx
+++ src/firebase/firebase.jsx
@@ -1,7 +1,10 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
-const auth = getAuth(app);
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCMli8iw2_IpuoDVZFiGBmvr0K2CZ5lDWY",
   authDomain: "cuadernodecomunicaciones-75199.firebaseapp.com",
@@ -11,8 +14,5 @@ const firebaseConfig = {
   appId: "1:13218076636:web:759a734dc099e5e133a119"
 };
 
-console.log(auth.currentUser);
-
-export { auth };
-
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
