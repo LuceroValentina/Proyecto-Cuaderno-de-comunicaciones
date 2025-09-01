187361b7a12d4d9f97c9b4c7efe5a461017fdaa8
LuceroValentina <lucero.valentina.epet20@gmail.com>
2025-09-01 15:54:30 -0300

    Subiendo correcciones de tamaño y posicion de botones en PantallaEdFisica

--- src/components/PantallaEducacionFisica.jsx
+++ src/components/PantallaEducacionFisica.jsx
@@ -18,10 +18,16 @@ const PantallaEducacionFisica = () => {
                 <button className="botonOscuro">Normas</button>
                 <button className="botonOscuro">Ficha médica</button>
             </div>
-            <div className="boton-volver">
-                <button className="volver">Volver</button>
-            </div>
-            <button onClick={() => navigate("/secciones")} className="absolute bottom-8 right-8 px-6 py-3 bg-[#3d6490] text-white rounded cursor-pointer">Volver</button>
+            <button
+                onClick={() => navigate("/secciones")}
+                className="volver fixed bottom-6 right-6 w-20 h-8 bg-[#3d6490] text-white rounded cursor-pointer flex items-center justify-center"
+            >
+                Volver
+            </button>
+
+
+
+
 
         </div>
     );
