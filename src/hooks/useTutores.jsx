42c7878415212e675849a75d3d4ee9e451a181fb
bri-ho <hernandez.brisa.epet20@gmail.com>
2025-09-10 20:30:55 -0300

    Subiendo hook useTutores

---
+++ src/hooks/useTutores.jsx
@@ -0,0 +1,19 @@
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../firebase/firebase';
+
+export const crearTutores = async ({ id, nombre, apellido, telefono, direccion, parentesco }) => {
+    try {
+        await addDoc(collection(db, 'usuarios'), {
+            id, 
+            nombre,
+            apellido,
+            telefono,
+            direccion, 
+            parentesco
+        });
+        return true;
+    } catch (error) {
+        console.error("Error al crear Tutor:", error);
+        return false;
+    }
+};
