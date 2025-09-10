b763a03c71becc242942f66d43de641292d786e9
bri-ho <hernandez.brisa.epet20@gmail.com>
2025-09-10 20:28:39 -0300

    Subiendo hook useAdmins

---
+++ src/hooks/useAdmins.jsx
@@ -0,0 +1,19 @@
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../firebase/firebase';
+
+export const crearAdmins = async ({ id, nombre, apellido, telefono, direccion, rol }) => {
+    try {
+        await addDoc(collection(db, 'usuarios'), {
+            id, 
+            nombre,
+            apellido,
+            telefono,
+            direccion, 
+            rol
+        });
+        return true;
+    } catch (error) {
+        console.error("Error al crear Admin:", error);
+        return false;
+    }
+};
