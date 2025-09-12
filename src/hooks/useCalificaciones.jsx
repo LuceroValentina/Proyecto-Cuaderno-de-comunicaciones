2b557a0a82a764f7b33d681be38291cf6ca3e755
Flor4a <sanchezflorencia2345@gmail.com>
2025-09-12 17:19:55 -0300

    Subiendo hook useCalificaciones

---
+++ src/hooks/useCalificaciones.jsx
@@ -0,0 +1,18 @@
+import {collection, addDoc} from 'firebase/firestore';
+import { db }from '.../firebase/firebase';
+
+export const crearCalificaciones = async ({ id, calificacion, texto  }) => {
+try {
+await addDoc(collection(db, 'calificaciones'), {//
+id,
+calificacion,
+texto
+});
+
+return true;
+} catch(error){
+console.error("Error al crear notas:", error);
+return false;
+}
+
+};
