891b35c4968158f0bf7fc10f85300ee5e19486f4
LuceroValentina <lucero.valentina.epet20@gmail.com>
2025-10-30 11:59:51 -0300

    Subiendo componente PantallaNormasEdfisica.jsx y su css

---
+++ src/components/PantallaNormasEdFisica.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "../css/PantallaNormasEdFisica.css";
+
+
+const PantallaNormasEdFisica = () => {
+
+    return (
+        <div className="container-normas">
+            <h1 className="titulo-edfisica">Normas de Educación Física</h1>
+            <h2 className="subtitulo-edfisica">Documentación</h2>
+            <ul className="lista-edfisica">
+                <li className="elementos-edfisica">Ficha médica y de aptitud física. Sera presentada a cada profesor/a no más allá de la 3era semana de iniciada la activdad. El cumplimiento en la entrega se considera como contenido a evaluar n el 1er trimestre.</li>
+                <li className="elementos-edfisica">Cuaderno de Comunicaciones. Siempre debe ser llevado a las clases y presetado si es solicitado por el/la docente.</li>
+                <li className="elementos-edfisica">Certificados médicos y/o justificaciones de inasistncias. Se presentarán en el térmico de 72 hs. de producida la inasistencia "original en preceptoría y copia al profesor/a."</li>
+                <li className="elementos-edfisica">Carpeta de la materia. Si se solicita debe ser llevada a clases siempre.</li>
+            </ul>
+            <h2 className="subtitulo-edfisica">Asistencia</h2>
+            <ul className="lista-edfisica">
+                <li className="elementos-edfisica">Los/las alumnas asistirán en horario contraturno de teoría en los días y horarios establecidos.</li>
+                <li className="elementos-edfisica">Se requiere un 70% de asistencia para aprobar la materia.</li>
+                <li className="elementos-edfisica">La justificación de inasistencia se realiza contra presentación en las 72hs. siguientes de certificado médico con la leyenda "reposo", y/o hasta 5 justificaciones al año escritas en cuaderno de comunicaciones solicitadas por padre, madre o tutor responsable del alumno/a.</li>
+                <li className="elementos-edfisica">El alumno/a por alguna razón no pueda realizar actividad física, deberá informarlo y asistir igualmente a la clase, en donde permanecerá cumpliendo el horario completo de la misma. Sera anotado "SAF" "presente sin actividad física".. La tardanza hasta 10' min. Se considera "tarde", pasado ese tiempo se considera "ausente", el/la alumna deberá permanecer en la clase y realizarla.</li>
+            </ul>
+            <h2 className="subtitulo-edfisica">Suspensión de actividades</h2>
+            <ul className="lista-edfisica">
+                <li className="elementos-edfisica">Por situaciones meteorológicas, frio o calor /menor a 0°C / mayor a 40°C/</li>
+                <li className="elementos-edfisica">Por precipitaciones /lluvia, granizo, nieve/</li>
+                <li className="elementos-edfisica">Emergencias por vientos fuertes</li>
+                <li className="elementos-edfisica">Otros causales, suelo mojado o resbaladizo, goteras, situaciones de deficiencia o peligro en las instalaciones.</li>
+                <li className="elementos-edfisica">En los casos que los estudiantes se encuentren en el establecimiento se retirarán con notificación en el cuaderno de comunicaciones, de no tenerlo, permanecerán en la escuela hasta cumplir el horario de clase.</li>
+                <li className="elementos-edfisica">Si la suspensión es resuelta por directivos de la escuela con tiempo antes de la clase, la misma será informada por los canales oficiales de comunicación que utiliza la institución.</li>
+            </ul>
+            <h2 className="subtitulo-edfisica">Vestimenta</h2>
+            <ul className="lista-edfisica">
+                <li >Se deberá asistir con ropa deportiva y zapatillas adecuadas para la actividad /buzo, calza larga, remera, bermuda, zapatillas, botines de papifútbol/.</li>
+                <li className="elementos-edfisica">Se prohíbe asistir con jeans, polleras, vestidos, trajes de baño, calzas cortas /tipo vóley/, musculosas bretel fino y escotadas, botines con tapones, alpargatas, zapatos, ojotas, crocs, etc. y ropa o calzado que el/la profesora justifique que no puede usarse para realizar la actividad.</li>
+                <li className="elementos-edfisica">Por seguridad propia y de los compañeros/as, está prohibido el uso durante la clase de accesorios tales como piercing, aros, colgantes, collares, muñequeras, pulseras, etc. o cualquier objeto que pueda provocar un daño físico propio o al de un compañero/a.</li>
+            </ul>
+            <h2 className="subtitulo-edfisica">Pautas de convivencia</h2>
+            <ul className="lista-edfisica">
+                <li className="elementos-edfisica">Se encuentran en total vigencia las pautas de convivencia y comportamiento de la institución.</li>
+            </ul>
+            <h2 className="subtitulo-edfisica">Accidentes</h2>
+            <ul className="lista-edfisica">
+                <li className="elementos-edfisica">En casos de accidentes se procederá bajo protocolo establecido.</li>
+            </ul>
+            <h2 className="subtitulo-edfisica">Docentes</h2>
+            <ul className="lista-edfisica">
+                <li className="elementos-edfisica">Los/las docentes presentarán a los/las alumnas el contrato pedagógico con las condiciones de aprobación de la materia y todo lo referido al dictado de la misma /T.P., evaluaciones, testeos, etc./.</li>
+                <li className="elementos-edfisica">El/la docente procurará informar con anterioridad su inasistencia a clase a la institución y los directivos informarán a los alumnos por los medios habituales de información.</li>
+            </ul>
+            <h2 className="subtitulo-edfisica">Generales</h2>
+            <ul className="lista-edfisica">
+                <li className="elementos-edfisica">El retiro de alumnos/as de la clase deberá ser realizado por padre, madre o tutor responsable.</li>
+                <li className="elementos-edfisica">Exenciones, por trabajo o deportivas, se realizarán bajo reglamentación existente.</li>
+                <li className="elementos-edfisica">Cambio de grupo u horario, deberá ser informado al profesor/a y autorizado por jefe de departamento.</li>
+                <li className="elementos-edfisica">Los elementos de educación física están encuadrados en las pautas de convivencia y comportamiento vigentes, y deben recibir el cuidado y uso adecuado para la actividad. En caso de rotura por utilización inadecuada deberá reponerse.</li>
+                <li className="elementos-edfisica">Los elementos personales de valor estarán bajo el total cuidado y responsabilidad de sus dueños. Está prohibido el uso de celulares y/o aparatos electrónicos durante la clase.</li>
+                <li className="elementos-edfisica">Cualquier cambio en las consideraciones anteriores, serán informadas por cuaderno de comunicaciones.</li>
+            </ul>
+        </div>
+
+    );
+
+};
+export default PantallaNormasEdFisica;
